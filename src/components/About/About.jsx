de3253e5c26b55bf34a168928bcf7bf8d30119ac
mohit8233 <mohitbanna085@gmail.com>
2025-09-22 13:42:52 +0530

    my-portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1fviczgVYX3fnzAtaiygG7_pyWK-LHOpc/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1fviczgVYX3fnzAtaiygG7_pyWK-LHOpc/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
